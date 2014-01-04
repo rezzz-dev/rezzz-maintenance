326df233d32672c427c952ebcb3a73bb8115b54c
Jason Resnick <jason@rezzz.com>
2014-01-04 12:40:00 -0500

    Plugin file created

---
+++ rezzz-maintenance.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: rezzz Maintenance
+Plugin URI: http://www.rezzz.com
+Description: Helper Plugin for the WordPress Maintenance provided by rezzz
+Version: 1.0
+Author: Jason Resnick
+Author URI: http://www.rezzz.com
+License: GPL2
+GitHub Plugin URI: https://github.com/rezzz-dev/rezzz-maintenance
+*/
